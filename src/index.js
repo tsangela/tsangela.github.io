1f6248fb018675ab79556bc79cb795121f6d87ae
Angela Tsang <tsangangela_@hotmail.com>
2020-10-12 21:12:35 -0700

    array destructure middleware

--- src/index.js
+++ src/index.js
@@ -7,13 +7,10 @@ import './index.css';
 import App from './app';
 import getReducers from './store/reducer';
 
+const middleware = process.env.NODE_ENV === 'development' ? [logger] : [];
+
 ReactDOM.render(
-  <Provider
-    store={createStore(
-      getReducers(),
-      applyMiddleware(process.env.NODE_ENV === 'development' && logger),
-    )}
-  >
+  <Provider store={createStore(getReducers(), applyMiddleware(...middleware))}>
     <App />
   </Provider>,
   document.getElementById('root'),
