617a7edc2da9a93ba395bc5f6fa5851511f6198a
Angela Tsang <tsangangela_@hotmail.com>
2020-12-11 17:51:07 -0800

    switch from BrowserRouter to HashRouter

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Route } from 'react-router';
 import { enquireScreen } from 'enquire-js';
 
@@ -28,7 +28,7 @@ function App() {
 
   return (
     <Layout>
-      <Router>
+      <Router basename="/">
         <NavigationBar isMobile={isMobile} tab={tab} setTab={setTab} />
         <Route
           exact
